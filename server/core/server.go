8c230c1aae3083d5d322232150d8eadd7032f8bd
Shaw1162084 <1162084@qq.com>
2026-01-24 17:09:53 +0800

    go启动优化，打印信息

--- server/core/server.go
+++ server/core/server.go
@@ -34,13 +34,19 @@ func RunServer() {
 
 	address := fmt.Sprintf(":%d", global.GVA_CONFIG.System.Addr)
 
+	//	fmt.Printf(`
+	//	欢迎使用 gin-vue-admin
+	//	当前版本:%s
+	//	默认自动化文档地址:http://127.0.0.1%s/swagger/index.html
+	//	默认MCP SSE地址:http://127.0.0.1%s%s
+	//	默认MCP Message地址:http://127.0.0.1%s%s
+	//	默认前端文件运行地址:http://127.0.0.1:8080
+	//`, global.Version, address, address, global.GVA_CONFIG.MCP.SSEPath, address, global.GVA_CONFIG.MCP.MessagePath)
+	//	initServer(address, Router, 10*time.Minute, 10*time.Minute)
+
 	fmt.Printf(`
 	欢迎使用 gin-vue-admin
 	当前版本:%s
-	默认自动化文档地址:http://127.0.0.1%s/swagger/index.html
-	默认MCP SSE地址:http://127.0.0.1%s%s
-	默认MCP Message地址:http://127.0.0.1%s%s
-	默认前端文件运行地址:http://127.0.0.1:8080
-`, global.Version, address, address, global.GVA_CONFIG.MCP.SSEPath, address, global.GVA_CONFIG.MCP.MessagePath)
+`, global.Version)
 	initServer(address, Router, 10*time.Minute, 10*time.Minute)
 }
